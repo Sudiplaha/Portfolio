e4b3f0a3968b604bc3c54f28efa6af99fd5ad56c
Sudip Laha <sudip@example.com>
2025-08-24 22:50:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function toggleAbout() {
+  const aboutSection = document.getElementById("about");
+  if (aboutSection.style.display === "none") {
+    aboutSection.style.display = "block";
+  } else {
+    aboutSection.style.display = "none";
+  }
+}
